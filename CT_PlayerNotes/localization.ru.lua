491430a8934c91149da9b51d892ee1a3dcae1f7b
M2us <Dead_Maus@mail.ru>
2017-06-19 20:41:16 +0300

    Added support ruRU locale

---
+++ CT_PlayerNotes/localization.ru.lua
@@ -0,0 +1,13 @@
+-- Version : Russian ( by Lichery )
+-- Last Update : 05/07/2016
+
+
+if ( GetLocale() == "ruRU" ) then
+
+CT_PLAYERNOTES_EDITING = "Редактирование заметки для игрока '%s'.";
+CT_PLAYERNOTES_CANCEL = "Отмена";
+CT_PLAYERNOTES_UPDATE = "Обновить";
+CT_PLAYERNOTES_EDITNOTE = "Редактор заметки";
+CT_PLAYERNOTES_CLICKEDIT = "Нажмите, чтобы изменить";
+
+end
